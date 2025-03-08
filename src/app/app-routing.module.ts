fab405d78a4b6375e0a883e90e5f7791d09c0d06
Datn7 <gdatunashvili@weinor.de>
2025-03-09 02:33:08 +0400

    components generation for menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SignupComponent } from './pages/signup/signup.component';
-import { NgModule } from '@angular/core';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'features', component: FeaturesComponent },
   { path: 'testimonials', component: TestimonialsComponent },
